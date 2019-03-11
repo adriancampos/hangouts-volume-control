f420f582318c442aa6fd72461f69708bea1e99e4
Adrian Campos <adriancampos@users.noreply.github.com>
2019-03-10 18:45:56 -0700

    Initial Commit

---
+++ content.js
@@ -0,0 +1,28 @@
+var container =document.querySelector('body > c-wiz > div > div.WOi1Wb > div.GhN39b > div > div > div.DQBC5d'); // TODO I should come up with a better selector
+var newElement = document.createElement("input");
+newElement.type="range";
+newElement.min="0";
+newElement.max="1";
+newElement.step="0.01";
+
+newElement.value = 1;
+
+newElement.onchange = function() {
+    setVolume(this.value);
+}
+
+container.insertBefore(newElement, container.firstChild);
+
+
+function getVolume() {
+    // TODO This returns the first audio element's volume. Is this okay?
+    document.querySelectorAll('audio').forEach(function(mitem) {
+        return mitem.volume;
+    })
+}
+
+function setVolume(volume) {
+    document.querySelectorAll('audio').forEach(function(mitem) {
+        mitem.volume = volume;
+    })
+}
